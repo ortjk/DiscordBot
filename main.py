c903d6c2a0f74a098210c05fdab324193763a744
ortjk <78326990+ortjk@users.noreply.github.com>
2021-07-28 16:05:35 -0230

    Update main.py

--- main.py
+++ main.py
@@ -37,18 +37,60 @@ class MyClient(discord.Client):
                 await message.channel.send(f"{message.author.mention} Invalid operators within '{split_message[1]} {split_message[2]} {split_message[3]}'")
 
         elif message.content.startswith('$roulette'):
-            try:
+            # try:
                 split_message = message.content.split(' ')
-                with open('roulette_profiles.txt', 'a') as f:
-                    allcontent = f.read().split('\n')
-                    for i in allcontent:
+                with open('roulette_profiles.txt', 'r+') as f:
+                    all_content = f.read().split('\n')
+                    exists = False
+                    user_location = 0
+                    print(all_content)
+                    for i in range(0, len(all_content)):
+                        if all_content[i].startswith(str(message.author.id)):
+                            exists = True
+                            user_location = i
+
+                    if not exists:
+                        all_content.append(f"{message.author.id} 100\n")
+                        user_location = len(all_content) - 1
 
                     if split_message[1].startswith('bet'):
+                        roll = random.randint(0, 36)
+                        user_score = all_content[user_location].split(' ')
 
-                    elif split_message[1].startswih('reset'):
+                        if roll > 18:
+                            await message.channel.send("rolled red...")
+                            if split_message[3] == 'red':
+                                all_content[user_location] = f"{message.author.id} {int(user_score[1]) + int(split_message[2])}\n"
+                                await message.channel.send("you win!")
+                            else:
+                                all_content[user_location] = f"{message.author.id} {int(user_score[1]) - int(split_message[2])}\n"
+                                await message.channel.send("you lose!")
 
-            except:
-                await message.channel.send(f"{message.author.mention} Invalid syntax within '{message.content}'. Syntax should be '$roulette <bet, reset> <amount> <colour (red, black, green)>'")
+                        elif roll > 0:
+                            await message.channel.send("rolled black...")
+                            if split_message[3] == 'black':
+                                all_content[user_location] = f"{message.author.id} {int(user_score[1]) + int(split_message[2])}\n"
+                                await message.channel.send("you win!")
+                            else:
+                                all_content[user_location] = f"{message.author.id} {int(user_score[1]) - int(split_message[2])}\n"
+                                await message.channel.send("you lose!")
+
+                        elif roll == 0:
+                            await message.channel.send("rolled green...")
+                            if split_message[3] == 'green':
+                                all_content[user_location] = f"{message.author.id} {int(user_score[1]) + int(split_message[2]) * 34}\n"
+                                await message.channel.send("you win!")
+                            else:
+                                all_content[user_location] = f"{message.author.id} {int(user_score[1]) - int(split_message[2])}\n"
+                                await message.channel.send("you lose!")
+
+                    elif split_message[1].startswith('reset'):
+                        all_content[user_location] = f"{message.author.id} 100\n"
+
+                    f.truncate(0)
+                    f.writelines(all_content)
+            # except:
+            #     await message.channel.send(f"{message.author.mention} Invalid syntax within '{message.content}'. Syntax should be '$roulette <bet, reset, or balance> <amount> <colour (red, black, green)>'")
 
         elif message.content.startswith('$quote'):
             with open('discordBot_quotes.txt', 'r') as file:
@@ -59,32 +101,37 @@ class MyClient(discord.Client):
         elif message.content.startswith('$kick'):
             split_message = message.content.split(' ', 1)
             try:
-                print(message.mentions)
+                mentioned_user = client.fetch_user(message.mentions[0].id)
+                mentioned_user = await mentioned_user
+                await message.guild.kick(mentioned_user, reason=split_message[1])
             except:
-                await message.channel.send(f"{message.author.mention} Invalid syntax within '{message.content}'. Syntax should be '$kick <mention user>'")
+                await message.channel.send(f"{message.author.mention} Invalid syntax within '{message.content}'. Syntax should be '$kick <mention user> <reason>'")
 
         elif message.content.startswith('$ban'):
             split_message = message.content.split(' ', 1)
             try:
-                print(message.mentions)
+                mentioned_user = client.fetch_user(message.mentions[0].id)
+                mentioned_user = await mentioned_user
+                await message.guild.ban(mentioned_user, reason=split_message[1], delete_message_days=0)
             except:
-                await message.channel.send(f"{message.author.mention} Invalid syntax within '{message.content}'. Syntax should be '$ban <mention user>'")
+                await message.channel.send(f"{message.author.mention} Invalid syntax within '{message.content}'. Syntax should be '$ban <mention user> <reason>'")
 
         elif message.content.startswith('$giverole'):
             try:
-                print(message.mentions)
-                print(message.role_mentions)
+                mentioned_member = message.guild.fetch_member(message.mentions[0].id)
+                mentioned_member = await mentioned_member
+                await mentioned_member.add_roles(message.role_mentions[0])
             except:
                 await message.channel.send(f"{message.author.mention} Invalid syntax within '{message.content}'. Syntax should be '$giverole <mention user> <mention role>'")
 
         elif message.content.startswith('$help'):
             await message.channel.send("""$whatis <number> <operator> <number>: Returns the result of the input mathematical equation
-            $quote: Return a random quote from a list of 100
-            $kick <mention user> <reason>: Kicks the mentioned user and sends them the reason
-            $ban <mention user> <reason>: Bans the mentioned user and sends them the reason
-            $giverole <mention user> <mention role>: Gives the mentioned user the mentioned role""")
+$quote: Return a random quote from a list of 100
+$kick <mention user> <reason>: Kicks the mentioned user with the given reason
+$ban <mention user> <reason>: Bans the mentioned user with the given reason
+$giverole <mention user> <mention role>: Gives the mentioned user the mentioned role""")
 
-        elif message.content.count('badword' or 'swearword' or 'notokword') != 0:
+        elif message.content.count('badword') != 0 or message.content.count('swearword') != 0 or message.content.count('notokword') != 0:
             await message.delete()
             await message.channel.send(f"{message.author.mention} said a bad word. The message has been deleted.")
 
